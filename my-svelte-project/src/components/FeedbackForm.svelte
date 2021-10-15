8e31e42a5805ca5201a756b8c9b8b42865b732a3
IshanPalkar <ishanpalkar3005@gmail.com>
2021-10-15 20:52:57 +0530

    commit on master

--- my-svelte-project/src/components/FeedbackForm.svelte
+++ my-svelte-project/src/components/FeedbackForm.svelte
@@ -1,11 +1,21 @@
 <script>
+import Card from './Card.svelte'
+import Button from './Button.svelte'
 
+let text = ''
 </script>
+
+
+<Card>
+    <header>
+        <h2>How would you like to rate our service with us?</h2>
+    </header>
+</Card>
 
 <form>
     <!--RATING SELECT-->
     <div class="input-group">
-        <input type="text" placeholder="Tell is something so that we can improve ourselves! Thankyou." >
-        <button>Send</button>
+        <input type="text" bind:value = {text} placeholder="Tell us something so that we can improve ourselves! Thankyou." >
+        <Button>Send</Button>
     </div>
 </form>
