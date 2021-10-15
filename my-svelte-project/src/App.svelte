df7a6a47d0e669f9f85ff7025a7c505b3fd9583f
IshanPalkar <ishanpalkar3005@gmail.com>
2021-10-15 17:44:19 +0530

    commit on master

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+import FeedbackItem from './components/FeedbackItem.svelte'
+import FeedbackList from './components/FeedbackList.svelte';
+import FeedbackStats from './components/FeedbackStats.svelte';
+import FeedbackStats from './components/FeedbackStats.svelte'
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: "lorem ipsum dolor sit amet, consectetur adipiscing.", 
+
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: "lorem ipsum dolor sit amet, consect"
+		},
+		{
+			id: 3,
+			rating: 7,
+			text: "lorem ipsum dolor sit amet, consect adipiscing sirac."
+		}
+	]
+	$:count = feedback.length
+	$:average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length
+
+	
+	const deleteFeedback = (e) => {
+	const itemId = e.detail
+	feedback = feedback.filter((item) => item.id != itemId)
+	}
+
+	 
+</script>
+
+<main class="container">
+	<FeedbackStats {count} {average} />
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
---
+++ my-svelte-project/src/components/FeedbackForm.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<form>
+    <!--RATING SELECT-->
+    <div class="input-group">
+        <input type="text" placeholder="Tell is something so that we can improve ourselves! Thankyou." >
+        <button>Send</button>
+    </div>
+</form>
