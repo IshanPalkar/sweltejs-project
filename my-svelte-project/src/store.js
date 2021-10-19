2ee5faf4a19e23c26a63c6c5f270b2ca3b624370
IshanPalkar <ishanpalkar3005@gmail.com>
2021-10-19 13:42:27 +0530

    commit on master

--- my-svelte-project/src/store.js
+++ my-svelte-project/src/store.js
@@ -3,7 +3,7 @@ import { writable } from 'svelte/store'
 export const FeedbackStore = writable([
     {
         id: 1,
-        rating: 2,
+        rating: 10,
         text: "lorem ipsum dolor sit amet, consectetur adipiscing.", 
 
     },
